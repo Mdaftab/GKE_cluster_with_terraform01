a818c1a1193d41243c72786f34d1d168afbe2403
Md Aftab <alam156@gmail.com>
2025-03-05 00:42:58 +0100

    fix: Update GKE module variables

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -5,11 +5,6 @@ variable "project_id" {
   type        = string
 }
 
-variable "cluster_name" {
-  description = "The name of the cluster"
-  type        = string
-}
-
 variable "region" {
   description = "The region to host the cluster in"
   type        = string
@@ -30,24 +25,42 @@ variable "subnet_name" {
   type        = string
 }
 
-variable "machine_type" {
-  description = "The machine type to use for the node pool"
-  type        = string
-  default     = "e2-micro"
-}
-
-variable "authorized_ip" {
-  description = "The IP address that will be allowed to access the cluster"
-  type        = string
-}
-
 variable "master_ipv4_cidr_block" {
-  description = "The IP range in CIDR notation for the master network"
+  description = "The IP range in CIDR notation to use for the hosted master network"
   type        = string
 }
 
-variable "cluster_viewers" {
-  description = "List of users/SAs that can view the cluster"
-  type        = list(string)
-  default     = []
+variable "service_account" {
+  description = "The service account to run nodes as"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "The machine type to use for nodes"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "min_node_count" {
+  description = "Minimum number of nodes in the NodePool"
+  type        = number
+  default     = 1
+}
+
+variable "max_node_count" {
+  description = "Maximum number of nodes in the NodePool"
+  type        = number
+  default     = 3
+}
+
+variable "initial_node_count" {
+  description = "Initial number of nodes in the NodePool"
+  type        = number
+  default     = 1
+}
+
+variable "environment" {
+  description = "The environment this cluster will run in"
+  type        = string
+  default     = "dev"
 }
